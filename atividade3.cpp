27c5005f6e5b35dddbc5fef5789ab6fadc7a86eb
Júlia Rosa <133364820+jjlia777@users.noreply.github.com>
2024-04-19 14:52:04 -0300

    Add files via upload

---
+++ atividade3.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+//1 – Peça para o usuário informar dois números, e veja se um
+//é maior que o outro.
+//- Se o primeiro número for maior, escreva, “o primeiro
+//número é maior”
+//- Se o segundo número for maior, escreva, “o segundo
+//número é maior”
+//- Senão, escreva, “os números são iguais”
+
+//int main() {
+//	int numero1;
+//	int numero2;
+//	cout << "Informe um numero: ";
+//	cin >> numero1;
+//	cout << "Informe outro numero: ";
+//	cin >> numero2;
+//
+//	if (numero1 > numero2) {
+//		cout << "o primeiro numero e maior";
+//	}
+//	else if (numero2 > numero1) {
+//		cout << "O segundo numero e maior";
+//	}
+//	else {
+//		cout << "Os numeros sao iguais";
+//	}
+//}
+
+
+
+//2 – Um mercado solicitou um sistema para avaliar a
+//quantidade de produtos no estoque.No sistema, o
+//funcionário deve informar a quantidade de produtos que
+//tem no estoque.
+//- Se o número for maior ou igual a 100, escreva “Quantidade
+//suficiente no estoque “
+//- Se o número for maior ou igual a 50, escreva “Atenção ao
+//estoque”
+//- Senão, escreva “Faça um novo pedido e reabasteça o
+//estoque”
+
+//int main() {
+//	int produtos;
+//	cout << "Qual a quantidade de produtos no estoque?: ";
+//	cin >> produtos;
+//
+//	if (produtos >= 100) {
+//		cout << "Quantidade suficiente no estoque";
+//	}
+//	else if (produtos >= 50) {
+//		cout << "Atencao no estoque";
+//	}
+//	else {
+//		cout << "Faca um novo pedido e reabasteca o estoque";
+//	}
+//}
+
+
+
+//3 – Peça para o usuário informar um número e veja onde ele
+//se encaixa :
+//-Se o número for maior que zero, escreva, “o número é
+//positivo”
+//- Se o número for menor que zero, escreva, “o número é
+//negativo”
+//- Senão, escreva, “o número é igual a zero”
+
+//int main() {
+//	int numero;
+//	cout << "Informe um numero e veja onde ele se encaixa : ";
+//	cin >> numero;
+//
+//	if (numero > 0) {
+//		cout << "O numero e positivo";
+//	}
+//	else if (numero < 0) {
+//		cout << "O numero e negativo";
+//	}
+//	else {
+//		cout << "O numero e igual a zero";
+//	}
+//}
